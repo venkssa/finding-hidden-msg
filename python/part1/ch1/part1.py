ddf4a904304fc49704cac3142d15139303ab63e8
Venkatesh Amudhan <venkatesh@ticketfly.com>
2016-07-08 00:46:48 -0700

    Finding Hidden Msg Part 1 Week 1 (ch1)

---
+++ python/part1/ch1/part1.py
@@ -0,0 +1,177 @@
+
+import math
+
+
+def _pattern_count(text, pattern):
+    counts = (1 for idx in range(0, len(text) - len(pattern) + 1) if text[idx:idx + len(pattern)] == pattern)
+    return sum(counts)
+
+
+def frequent_words(text, k):
+    kmers = set(text[idx:idx + k] for idx in range(0, len(text) - k + 1))
+    kmer_count = [(kmer, _pattern_count(text, kmer)) for kmer in kmers]
+    max_count = max(kmer_count, key=lambda ck: ck[1])[1]
+    return set((kmer for kmer, count in kmer_count if count == max_count))
+
+
+_nbase = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+_rbase = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
+
+
+def _pattern_to_number(pattern):
+    value = 0
+
+    for ch in pattern:
+        value = value * 4 + _nbase[ch]
+
+    return value
+
+
+def _number_to_pattern(number, k):
+    pattern = ['A' for _ in range(k)]
+
+    idx = k - 1
+    while number > 0:
+        pattern[idx] = _rbase[number % 4]
+        number = math.floor(number / 4)
+        idx -= 1
+
+    return "".join(pattern)
+
+
+def _computing_frequencies(text, k):
+    freq = [0 for _ in range(0, 4 ** k)]
+
+    for kmer in (text[idx:idx + k] for idx in range(0, len(text) - k + 1)):
+        num = _pattern_to_number(kmer)
+        freq[num] += 1
+
+    return freq
+
+
+def faster_frequent_words(text, k):
+    freq = _computing_frequencies(text, k)
+    max_count = max(freq)
+    return set((_number_to_pattern(num, k) for num, count in enumerate(freq) if count == max_count))
+
+
+def faster_frequent_words_by_sorting(text, k):
+    encoded_kmers = sorted([_pattern_to_number(text[idx:idx + k]) for idx in range(0, len(text) - k + 1)])
+    counts = [1 for _ in range(0, len(encoded_kmers))]
+
+    for first, second in zip(enumerate(encoded_kmers), enumerate(encoded_kmers[1:], 1)):
+        if first[1] == second[1]:
+            counts[second[0]] += counts[first[0]]
+
+    max_count = max(counts)
+    return set((_number_to_pattern(kmer, k) for kmer, count in zip(encoded_kmers, counts) if count == max_count))
+
+
+_complement = str.maketrans('ATGC', 'TACG')
+
+
+def reverse_complement(pattern):
+    return pattern.translate(_complement)[::-1]
+
+
+def pattern_matching(pattern, genome):
+    """
+     Find all occurrences of a pattern in a string.
+
+    :return: All starting positions in Genome where Pattern appears as a substring.
+    """
+    occurrences = []
+    idx = genome.find(pattern)
+    while idx != -1:
+        occurrences.append(idx)
+        idx = genome.find(pattern, idx + 1)
+    return occurrences
+
+
+def find_clumps_slow(genome, k, el, t):
+    """
+    Find patterns forming clumps in a string.
+    :param genome:
+    :param k: length of k-mer
+    :param el: length of window
+    :param t: min num of appearances for selection
+    :return: All distinct k-mers forming (el, t)-clumps in Genome.
+    """
+    genome_slices = (genome[idx:idx + el] for idx in range(0, len(genome) - el))
+    frequencies = (_computing_frequencies(genome_slice, k) for genome_slice in genome_slices)
+    return set((_number_to_pattern(number, k) for frequency in frequencies
+                for (number, freq_count) in enumerate(frequency) if freq_count >= t))
+
+
+class _FreqArray(object):
+
+    def __init__(self, t):
+        self._freq_count = {}
+        self._clumps = set()
+        self._t = t
+
+    def dec_count(self, key):
+        count = self._freq_count.get(key)
+        if count is None or count == 0:
+            raise KeyError("Cannot decrement {} as it does not exist in freq_count".format(key))
+        if count == self._t:
+            self._clumps.remove(key)
+        if count == 1:
+            self._freq_count.pop(key)
+        else:
+            self._freq_count[key] = count - 1
+
+    def inc_count(self, key):
+        self._freq_count[key] = self._freq_count.get(key, 0) + 1
+        if self._freq_count[key] == self._t:
+            self._clumps.add(key)
+
+    def count(self, key):
+        return self._freq_count.get(key, 0)
+
+    def kmers_in_clumps(self):
+        return self._clumps
+
+
+def _genome_to_numeric_genome_convertor(genome, k):
+    numeric_genome = []
+    value = 0
+    for ch in genome[0:k]:
+        value = value * 4 + _nbase[ch]
+    numeric_genome.append(value)
+
+    p = 4 ** (k - 1)
+    for prev, next in zip(genome[0:len(genome) - k], genome[k:]):
+        value -= (p * _nbase[prev])
+        value = value * 4 + _nbase[next]
+        numeric_genome.append(value)
+
+    return numeric_genome
+
+
+def find_clumps(genome, k, el, t):
+    """
+    Find patterns forming clumps in a string.
+    :param genome:
+    :param k: length of k-mer
+    :param el: length of window
+    :param t: min num of appearances for selection
+    :return: All distinct k-mers forming (el, t)-clumps in Genome.
+    """
+    numeric_genome = _genome_to_numeric_genome_convertor(genome, k)
+    
+    nel = el - k + 1
+
+    freq_count = _FreqArray(t)
+
+    for number in numeric_genome[0:el]:
+        freq_count.inc_count(number)
+
+    clumps = set(freq_count.kmers_in_clumps())
+
+    for prev, next in zip(numeric_genome[0: len(numeric_genome) - nel], numeric_genome[nel:]):
+        freq_count.dec_count(prev)
+        freq_count.inc_count(next)
+        clumps.update(freq_count.kmers_in_clumps())
+
+    return set((_number_to_pattern(kmer, k) for kmer in clumps))
---
+++ python/part1/ch1/part1_test.py
@@ -0,0 +1,64 @@
+
+import unittest
+
+from ch1.part1 import *
+from ch1.part1 import _pattern_count, _computing_frequencies, _number_to_pattern, _pattern_to_number
+
+
+class Part1Tests(unittest.TestCase):
+
+    def test_valid_pattern_count(self):
+        self.assertEqual(_pattern_count("ACAACTATGCATACTATCGGGAACTATCCT", "ACTAT"), 3)
+        self.assertEqual(_pattern_count("GCGCG", "GCG"), 2)
+
+    def test_no_valid_pattern_count(self):
+        self.assertEqual(_pattern_count("ACAT", "GCT"), 0)
+
+    def test_frequent_words(self):
+        self.assertSetEqual(frequent_words("ACAACTATGCATACTATCGGGAACTATCCT", 5), {"ACTAT"})
+        self.assertSetEqual(frequent_words("CGATATATCCATAG", 3), {"ATA"})
+        self.assertSetEqual(frequent_words("ACGTTGCATGTCGCATGATGCATGAGAGCT", 4), {"CATG", "GCAT"})
+
+    def test_pattern_to_number(self):
+        self.assertEqual(_pattern_to_number("AAAA"), 0)
+        self.assertEqual(_pattern_to_number("AAAC"), 1)
+        self.assertEqual(_pattern_to_number("AAGA"), 8)
+
+    def test_number_to_pattern(self):
+        self.assertEqual(_number_to_pattern(8, 4), "AAGA")
+        self.assertEqual(_number_to_pattern(0, 4), "AAAA")
+        self.assertEqual(_number_to_pattern(1, 4), "AAAC")
+
+        self.assertEqual(_number_to_pattern(8, 2), "GA")
+        self.assertEqual(_number_to_pattern(0, 2), "AA")
+        self.assertEqual(_number_to_pattern(1, 2), "AC")
+
+    def test_computing_frequencies(self):
+        self.assertListEqual(_computing_frequencies("ACGCGGCTCTGAAA", 2),
+                             [2, 1, 0, 0, 0, 0, 2, 2, 1, 2, 1, 0, 0, 1, 1, 0])
+
+    def test_faster_frequent_words(self):
+        self.assertSetEqual(faster_frequent_words("ACAACTATGCATACTATCGGGAACTATCCT", 5), {"ACTAT"})
+        self.assertSetEqual(faster_frequent_words("CGATATATCCATAG", 3), {"ATA"})
+        self.assertSetEqual(faster_frequent_words("ACGTTGCATGTCGCATGATGCATGAGAGCT", 4), {"CATG", "GCAT"})
+
+    def test_faster_frequent_words_by_sorting(self):
+        self.assertSetEqual(faster_frequent_words_by_sorting("ACAACTATGCATACTATCGGGAACTATCCT", 5), {"ACTAT"})
+        self.assertSetEqual(faster_frequent_words_by_sorting("CGATATATCCATAG", 3), {"ATA"})
+        self.assertSetEqual(faster_frequent_words_by_sorting("ACGTTGCATGTCGCATGATGCATGAGAGCT", 4), {"CATG", "GCAT"})
+
+    def test_reverse_complement(self):
+        self.assertEqual(reverse_complement("AAAACCCGGT"), "ACCGGGTTTT")
+
+    def test_pattern_matching(self):
+        self.assertListEqual(pattern_matching("ATAT", "GATATATGCATATACTT"), [1, 3, 9])
+
+    def test_find_clumps_slow(self):
+        self.assertSetEqual(
+            find_clumps_slow("CGGACTCGACAGATGTGAAGAACGACAATGTGAAGACTCGACACGACAGAGTGAAGAGAAGAGGAAACATTGTAA", k=5, el=50, t=4),
+            {"CGACA", "GAAGA"})
+
+    def test_find_clumps(self):
+        self.assertSetEqual(
+            find_clumps("CGGACTCGACAGATGTGAAGAACGACAATGTGAAGACTCGACACGACAGAGTGAAGAGAAGAGGAAACATTGTAA", k=5, el=50, t=4),
+            {"CGACA", "GAAGA"})
